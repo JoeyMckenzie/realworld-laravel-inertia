67c8e014d3337df184ae0a444b299fb2759b3401
Joey McKenzie <joey.mckenzie27@gmail.com>
2023-09-07 22:20:02 -0700

    chore: add larastan

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request): void
+    {
+
+    }
+}
